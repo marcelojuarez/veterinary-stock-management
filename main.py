8a9a445bb6742572c751841023259c3399c21e4f
juancreynoso <juancreynoso11@gmail.com>
2025-09-09 19:40:59 -0300

    database modified

--- main.py
+++ main.py
@@ -2,17 +2,10 @@ from views.main_view import App
 from db.database import db
 
 def main():
-
     try:
-        
         print("establishing connection to the database")
         db.create_tables()
         print("database initialized")
-
-        query = "INSERT INTO stock (id, name) VALUES (?, ?)"
-        new_id = db.execute_query(query, (1, "Marcelo"))
-        print(new_id)  # Por ejemplo: 7
-
 
         print("initializing app")
         app = App()
@@ -20,7 +13,6 @@ def main():
 
         app.run()
         
-
     except Exception as e:
         print(f"Error al iniciar la aplicación: {e}")
         import traceback
