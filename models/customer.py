57a3116d0e304d63c8fdb1c996273e2d33de4250
agusc546 <aguscesari14@gmail.com>
2026-03-14 18:42:58 -0300

    modified is_cash_sale

--- models/customer.py
+++ models/customer.py
@@ -383,7 +383,7 @@ class CustomerModel:
 
         return norm_to_2_dec(total_pending)
 
-    def _is_cash_sale(self, sale_id):
+    def _is_cash_sale(self, sale_id, date):
         """
         Determina si una venta fue de CONTADO.
         
